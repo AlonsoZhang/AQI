b22c8274ba8ca58fc51ebe0e468ff4ada74b33a6
AlonsoZhang <code@zhangwu.tech>
2018-04-16 09:51:50 +0800

    Remove viewcontroller

--- AQI/AppDelegate.swift
+++ AQI/AppDelegate.swift
@@ -2,25 +2,51 @@
 //  AppDelegate.swift
 //  AQI
 //
-//  Created by Alonso on 2018/4/13.
+//  Created by Alonso on 2018/4/16.
 //  Copyright © 2018 Alonso. All rights reserved.
 //
 
 import Cocoa
+import Ji
+import Alamofire
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        Alamofire.request("http://222.92.42.178:8083/AQI/Index.aspx").responseString { response in
+            if(response.result.isSuccess){
+                self.dealHTML(Str: response.description)
+                //print(response.description)
+            }else{
+                print("Check network")
+            }
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    func dealHTML(Str:String) {
+        let jiDoc = Ji(htmlString: Str, encoding: .utf8)
+        //let jiDoc = Ji(htmlURL: URL(string: "http://222.92.42.178:8083/AQI/Index.aspx")!)
+        let spanDate = jiDoc?.xPath("//span[@id='ContentPlaceHolder1_spanDate']")?.first?.content
+        print("更新时间:\(spanDate!)")
+        let spanAQI = jiDoc?.xPath("//span[@id='ContentPlaceHolder1_spanAQI']")?.first?.content
+        print("空气质量指数(AQI):\(spanAQI!)")
+        let spanClass = jiDoc?.xPath("//span[@id='ContentPlaceHolder1_spanClass']")?.first?.content
+        let spanGrade = jiDoc?.xPath("//span[@id='ContentPlaceHolder1_spanGrade']")?.first?.content
+        print("等级:\(spanClass!) \(spanGrade!)")
+        let spanHealthEffect = jiDoc?.xPath("//span[@id='ContentPlaceHolder1_spanHealthEffect']")?.first?.content
+        print("对健康的影响:\(spanHealthEffect!)")
+        let spanTakeStep = jiDoc?.xPath("//span[@id='ContentPlaceHolder1_spanTakeStep']")?.first?.content
+        print("建议采取的措施:\(spanTakeStep!)")
+        let spanPrimaryPollutant = jiDoc?.xPath("//span[@id='ContentPlaceHolder1_spanPrimaryPollutant']")?.first?.content
+        let spanValue = jiDoc?.xPath("//span[@id='ContentPlaceHolder1_spanValue']")?.first?.content
+        print("首要污染物:\(spanPrimaryPollutant!) \(spanValue!)")
+    }
 
 }
 
